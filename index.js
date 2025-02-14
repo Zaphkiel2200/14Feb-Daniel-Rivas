77b539dfe6d747b3de2545a1ca44fdb1e5250581
Zaphkiel2200 <135277854+Zaphkiel2200@users.noreply.github.com>
2025-02-14 17:25:55 -0500

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+class animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.paws = 4;
+  }
+}
+
+sound() {
+    console.log('An animal sound');
+    return 'An animal sound';
+    }
